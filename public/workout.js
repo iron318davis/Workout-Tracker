4cdd98d37b56f26b0387062c428970941ee1663f
Davis Sjoberg <59490187+iron318davis@users.noreply.github.com>
2020-07-28 23:07:30 -0500

    fixed total workout duration

--- public/workout.js
+++ public/workout.js
@@ -21,6 +21,8 @@ async function initWorkout() {
 
 function tallyExercises(exercises) {
   const tallied = exercises.reduce((acc, curr) => {
+    // fixing broken duration on both types of workouts
+    acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
     if (curr.type === "resistance") {
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
